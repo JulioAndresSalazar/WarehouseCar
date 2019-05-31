2a141a88f22bb208cbcf8721a88dfe60ee5090cb
JulioAndresSalazar <48372550+JulioAndresSalazar@users.noreply.github.com>
2019-05-31 11:53:53 -0400

    Update ClientCode1.0.py

--- ClientCode1.0.py
+++ ClientCode1.0.py
@@ -1,19 +1,16 @@
+#This script sends data to server pi from a client Pi and waits for a reply.
 import socket
-
-# Enter IP address of the pi you are talking to, e.g. 10.245.150.164
-IP_address = '10.245.158.79' 
+IP_address = '10.245.158.79' #Enter the IP Address of the client Pi
 PORT_num = 2460 
 BUFFER = 1024
 
 def sendData(msg):
-    # connect to server, send message, get back response
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # set up socket
+    #Connect to server, send message, get back response
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     s.connect((IP_address, PORT_num))
-    s.sendall(message) # send message
-    data = s.recv(BUFFER) # get back response
-    s.close() # tells the server that the connection is done
-    
-    # print what was returned by server
+    s.sendall(message)
+    data = s.recv(BUFFER)
+    s.close() 
     print(data)
 
 message_pre = '11N'
